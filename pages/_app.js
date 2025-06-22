78731f4540039022f4395d8d8a28ec00e32be797
hemyar143 <askarhemyar@yahoo.com>
2025-06-22 12:44:22 +0000

    Add Vercel Speed Insights to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import { supabase } from '../utils/supabaseClient';
+import { inject } from '@vercel/speed-insights';
+
+inject(); // <-- Add this line at the top
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const { data: listener } = supabase.auth.onAuthStateChange(() => {});
-    return () => listener?.subscription.unsubscribe();
+    return () => listener.subscription.unsubscribe();
   }, []);
 
   return <Component {...pageProps} />;
